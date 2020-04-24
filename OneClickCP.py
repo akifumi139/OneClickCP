f7638f5b2833909a192b277f1e4fdb75c106eca4
akifumi139 <akifumi139@gmail.com>
2020-04-25 00:58:57 +0900

    動くようにしたよ

--- OneClickCP.py
+++ OneClickCP.py
@@ -1,4 +1,4 @@
-import bpy
+import bpy,os,datetime
 
 class CheckPoint_PT_ui (bpy.types.Panel):
     bl_label = "Check Point"
@@ -12,29 +12,29 @@ class CheckPoint_PT_ui (bpy.types.Panel):
         scene = context.scene
         
         row =layout.row()
-        row.prop(scene, "self_rename")
+        row.prop(scene, "named")
         
         row =layout.row()
         row.prop(scene, "file_name")
         
         row =layout.row()
-        row.prop(scene, "overwrite")
+        row.label(text = "description" ,icon="DOCUMENTS")
         
         row =layout.row()
         row.prop(scene, "description")
         
-        row =layout.row()
-        row.label(text = "description" ,icon="DOCUMENTS")
-        
         row = layout.row()
         self.layout.operator("save.button",icon= 'GREASEPENCIL')
+        
+        #row.prop(scene, "overwrite")
+        
 
 class Save_OT_button(bpy.types.Operator):
     bl_label = "Save"
     bl_idname = "save.button"
 
     def execute(self, context):
-        print("pushed")
+        save_func()
         return{'FINISHED'}
     
 regist_classes = (
@@ -43,8 +43,8 @@ regist_classes = (
 )    
         
 def register():
-    bpy.types.Scene.self_rename = bpy.props.BoolProperty(
-        name="Self rename",
+    bpy.types.Scene.named = bpy.props.BoolProperty(
+        name="named",
         default = False)
     bpy.types.Scene.file_name = bpy.props.StringProperty(name="File name",
                              default="",
@@ -64,3 +64,41 @@ def unregister():
    
 if __name__ == "__main__":
     register()
+
+
+
+
+def save_func():
+    
+    filename = os.path.splitext(os.path.basename(bpy.data.filepath))[0]
+    if(filename==''):
+        print("Please save the file you are working on and then execute")
+        return
+    
+    
+    foldername= filename+'_blend'
+
+    try:
+        os.mkdir(foldername)
+        f = open(foldername+"/cp_version.txt", "w")
+        f.close()
+    except FileExistsError as e:
+        print(e.strerror)
+    
+    
+    if(bpy.context.scene.named):
+        path=bpy.context.scene.file_name+dt_now+".blend"
+    else:
+        dt_now = datetime.datetime.now().strftime('%Y_%m_%d_%H:%M:%S')
+        path="filename"+dt_now+".blend"
+
+    f = open(foldername+"/cp_version.txt", "a")
+    
+    f.write(path+" : ")
+    if(bpy.context.scene.description != ""):
+        f.write(bpy.context.scene.description)
+        
+    f.write("\n\n")
+    f.close()
+    
+    bpy.ops.wm.save_as_mainfile(copy=True,filepath=foldername+"/"+path)
